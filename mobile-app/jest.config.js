2494e163d4e443dc660dde7f97f5c20a64f9be30
William Mccoy <williammccoy2012@gmail.com>
2026-03-30 13:49:25 -0500

    chore: refresh dependency security fixes

--- mobile-app/jest.config.js
+++ mobile-app/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'react-native',
-  testEnvironment: 'jsdom',
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|react-native-vector-icons|@react-navigation|@walletconnect)/)',
   ],
