516bafefa03bde51abe2b1f9d492975632d26128
William Mccoy <williammccoy2012@gmail.com>
2026-04-14 13:44:03 -0500

    Fix __dirname for ES modules

--- smart-contract/scripts/deploy-treasury.js
+++ smart-contract/scripts/deploy-treasury.js
@@ -6,6 +6,10 @@ const { ethers } = hre;
 import { deployUupsProxy, getImplementationAddress } from "../utils/uups.mjs";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const Networks = {
   polygon: 137,
